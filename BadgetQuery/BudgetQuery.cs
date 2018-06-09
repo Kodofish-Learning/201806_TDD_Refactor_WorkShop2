5ec38b869065f3accaed773848e9e4662039bbea
Kodofish Chang <kodofish.chang@johnsontechinc.com>
2018-06-10 01:19:37 +0800

    Complete 3rd test case, get green

--- BadgetQuery/BudgetQuery.cs
+++ BadgetQuery/BudgetQuery.cs
@@ -15,14 +15,41 @@ namespace BadgetQuery
         public decimal Query(DateTime startDate, DateTime endDate)
         {
             var budgets = _budgetRepostory.GetBudgets();
+            if (startDate.Year == endDate.Year && startDate.Month != endDate.Month)
+            {
+                var totalBudget = 0m;
+
+
+                var firstMonthStartDate = startDate;
+                var firstMonthEndDate = new DateTime(startDate.Year, startDate.Month, DateTime.DaysInMonth(startDate.Year, startDate.Month));
+
+                var dayDiff = new TimeSpan(firstMonthEndDate.Ticks - firstMonthStartDate.Ticks).Days + 1;
+                var currentMonthBudget = budgets.FirstOrDefault(it => it.YearMonth == new DateTime(firstMonthStartDate.Year, firstMonthStartDate.Month, 1))?.Amount ?? 0m;
+                var currentMonthDays = DateTime.DaysInMonth(firstMonthStartDate.Year, firstMonthStartDate.Month);
+                var oneDayBudgetAmount = currentMonthBudget / currentMonthDays;
+                totalBudget += dayDiff * oneDayBudgetAmount;
+
+                var secondMonthStartDate = new DateTime(endDate.Year, endDate.Month, 1);
+                var secondMonthEndDate = endDate;
+
+                dayDiff = new TimeSpan(secondMonthEndDate.Ticks - secondMonthStartDate.Ticks).Days + 1;
+                currentMonthBudget = budgets.FirstOrDefault(it => it.YearMonth == new DateTime(secondMonthStartDate.Year, secondMonthStartDate.Month, 1))?.Amount ?? 0m;
+                currentMonthDays = DateTime.DaysInMonth(secondMonthStartDate.Year, secondMonthStartDate.Month);
+                oneDayBudgetAmount = currentMonthBudget / currentMonthDays;
+                totalBudget += dayDiff * oneDayBudgetAmount;
+
+                return totalBudget;
+            }
+
             if (startDate.Month == endDate.Month && startDate.Year == endDate.Year)
             {
-                var dayDiff = new TimeSpan(endDate.Ticks-startDate.Ticks).Days+1;
+                var dayDiff = new TimeSpan(endDate.Ticks - startDate.Ticks).Days + 1;
                 var currentMonthBudget = budgets.First(it => it.YearMonth == new DateTime(startDate.Year, startDate.Month, 1));
                 var currentMonthDays = DateTime.DaysInMonth(startDate.Year, startDate.Month);
                 var oneDayBudgetAmount = currentMonthBudget.Amount / currentMonthDays;
                 return dayDiff * oneDayBudgetAmount;
             }
+
 
             return 10m;
         }
