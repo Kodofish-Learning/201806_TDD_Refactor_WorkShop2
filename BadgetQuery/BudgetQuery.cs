d43b4cd520d50f3b3cf83e65b7a80df8aea22d3a
Kodofish Chang <kodofish.chang@johnsontechinc.com>
2018-06-10 01:58:56 +0800

    refactor code, 將跨月處理時, 處理 頭/尾 月份都移到迴圈裡

--- BadgetQuery/BudgetQuery.cs
+++ BadgetQuery/BudgetQuery.cs
@@ -19,24 +19,26 @@ namespace BadgetQuery
             if (startDate.Year == endDate.Year && startDate.Month != endDate.Month)
             {
                 var totalBudget = 0m;
-                var monthList = Enumerable.Range(startDate.Month, endDate.Month - startDate.Month).ToList();
+                var monthList = Enumerable.Range(startDate.Month, endDate.Month - startDate.Month + 1).ToList();
 
-                var firstMonthStartDate = startDate;
-                var firstMonthEndDate = new DateTime(startDate.Year, startDate.Month, DateTime.DaysInMonth(startDate.Year, startDate.Month));
-                monthList.Remove(startDate.Month);
-
-                totalBudget += QueryInMonthBudgetAmount(firstMonthStartDate, firstMonthEndDate, budgets);
-
-                var secondMonthStartDate = new DateTime(endDate.Year, endDate.Month, 1);
-                var secondMonthEndDate = endDate;
-                monthList.Remove(endDate.Month);
-
-                totalBudget += QueryInMonthBudgetAmount(secondMonthStartDate, secondMonthEndDate, budgets);
 
                 foreach (var month in monthList)
                 {
                     var monthStartDate = new DateTime(startDate.Year, month, 1);
                     var monthEndDate = new DateTime(startDate.Year, month, DateTime.DaysInMonth(startDate.Year, month));
+
+                    if (month == monthList.First())
+                    {
+                        monthStartDate = startDate;
+                        monthEndDate = new DateTime(startDate.Year, month, DateTime.DaysInMonth(startDate.Year, month));
+                    }
+
+                    if (month == monthList.Last())
+                    {
+                        monthStartDate = new DateTime(startDate.Year, month, 1);
+                        monthEndDate = endDate;
+                    }
+
                     totalBudget += QueryInMonthBudgetAmount(monthStartDate, monthEndDate, budgets);
                 }
 
